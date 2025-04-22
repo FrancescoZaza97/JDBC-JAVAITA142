8d3bfffdb9684a52c0ba066db03028a24a5c97dd
Checco <francesco.zaza0510@gmail.com>
2025-04-22 09:47:54 +0200

    metodi SQL

---
+++ Esercizio1/src/select.java
@@ -0,0 +1,41 @@
+// programma per acquisire dati da una tabella usando JDBC
+
+import java.sql.*;
+
+public class select {
+	
+	// url che punta al database mysql
+    static final String url = "jdbc:mysql://localhost:3306/rubrica";
+	
+	public static void main (String args[]) {
+		
+		try {
+			// metodo usato per la registrazione del driver utilizzato			
+			Class.forName("com.mysql.cj.jdbc.Driver");	
+			
+			// getConnection() stabilisce una connessione, passando l'url che punta al database, lo username e la password
+			Connection conn = DriverManager.getConnection (url, "root", "");
+			
+			// Creiamo un oggetto Statement per poter interrogare il db
+			Statement cmd = conn.createStatement();
+       
+			// executeQuery(String query) invia la richiesta di acquisizione al database e ne salva il return in un oggett ResultSet
+			ResultSet res = cmd.executeQuery("SELECT * FROM anagrafica_clienti");
+			
+			// Stampiamone i risultati riga per riga
+			while (res.next()) {
+				System.out.println(res.getString("id_anagrafica"));
+				System.out.println(res.getString("ragione_sociale"));
+				System.out.println(res.getString("indirizzo"));
+				System.out.println(res.getString("cap"));
+				System.out.println(res.getString("localita"));
+				System.out.println(res.getString("provincia"));
+			}					
+				
+			conn.close();
+
+		}
+		catch (SQLException | ClassNotFoundException e) { }
+		
+	}
+}
